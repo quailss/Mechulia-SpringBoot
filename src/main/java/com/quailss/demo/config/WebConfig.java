c96cef33010673723386f4c4249d8ff8d1605f6d
karam Choi <riverchoi2523@gmail.com>
2024-09-09 22:03:29 +0900

    [REMOVE] 불필요한 import문 삭제

--- src/main/java/com/quailss/demo/config/WebConfig.java
+++ src/main/java/com/quailss/demo/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.quailss.demo.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
